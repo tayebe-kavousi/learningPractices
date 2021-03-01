8e0191bccca2bd4719e02c6ebaa951495db66049
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-03-01 13:48:40 +0800

    Find window coordinates of the field js

---
+++ coordinats.js
@@ -0,0 +1,20 @@
+let elem = document.getElementById('field');
+  
+    
+let coord = elem.getBoundingClientRect();
+
+let topLeftOuterX = coord.left;
+let topLeftOuterY= coord.top;
+console.log(`1= (${topLeftOuterX} : ${topLeftOuterY})`);
+
+let topLeftInnerX= +coord.left + +parseInt(getComputedStyle(elem).borderLeftWidth) ;
+let topLeftInnerY= +coord.top + +parseInt(getComputedStyle(elem).borderTopWidth) ;
+console.log(`3= (${topLeftInnerX} : ${topLeftInnerY})`);
+
+let bottomRightInnerX = +coord.right - parseInt(getComputedStyle(elem).borderRightWidth) ;
+let bottomRightInnerY = coord.bottom - parseInt(getComputedStyle(elem).borderBottomWidth);
+console.log(`4= (${bottomRightInnerX} : ${bottomRightInnerY})`);
+
+let bottomRightOuterY = coord.bottom ;
+let bottomRightOuterX =coord.right ;
+console.log(`2= (${bottomRightOuterX} : ${bottomRightOuterY})`);
