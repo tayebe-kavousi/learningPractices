408ed315bc33d24fbf3485488c24f431b34a0477
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-03-31 09:39:11 +0800

    adding delete all feature

--- toDoList/app.js
+++ toDoList/app.js
@@ -2,20 +2,36 @@
 $(function(){
     $('#add').on("click", function(){
         let val = $('#input').val();
+        let tasks;
         if (val != ''){
-            let elem = $('<li></li>').html(val);
+            let elem = $('<li class="toDo"></li>').html(val);
             elem.append('<i class=" trash far fa-trash-alt"></i>');
             elem.append('<i class=" check fas fa-check"></i>');
             $('#myList').append(elem);
             $('#input').val('');
+            tasks = $('li').get();
+            console.log(tasks);
+            if(tasks.length > 1){
+                $('.deleteAll').removeClass('hidden');
+            }
             $('.trash').on('click', function(){
                 $(this).parent().remove();
+                tasks.pop();
+                console.log(tasks);
+                if(tasks.length < 2){
+                    $('.deleteAll').addClass('hidden');
+                }
             });
             $('.check').on('click', function(){
-                $(this).css('color', 'green');
-                $(this).parent().css('color', 'green');
+                $(this).toggleClass('checked');
+                $(this).parent().toggleClass('done');
                 $('.trash').css('color', 'black')
             });
-        }
+        }   
+        
+        $('.deleteAll').click(function(){
+            $('li').remove();
+            $(this).addClass('hidden');
+        })
     });
 });
