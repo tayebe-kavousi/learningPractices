fa949e8c264702dcdd041932f377559b592a57e8
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-03-31 11:11:57 +0800

    fixing functionality

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,37 +1,39 @@
 
 $(function(){
-    $('#add').on("click", function(){
+    let tasks;
+
+    $('#add').click(function(){
         let val = $('#input').val();
-        let tasks;
         if (val != ''){
             let elem = $('<li class="toDo"></li>').html(val);
-            elem.append('<i class=" trash far fa-trash-alt"></i>');
-            elem.append('<i class=" check fas fa-check"></i>');
+            elem.append('<i class="trash far fa-trash-alt"></i>');
+            elem.append('<i class="check fas fa-check"></i>');
             $('#myList').append(elem);
             $('#input').val('');
+
             tasks = $('li').get();
-            console.log(tasks);
             if(tasks.length > 1){
                 $('.deleteAll').removeClass('hidden');
             }
-            $('.trash').on('click', function(){
-                $(this).parent().remove();
-                tasks.pop();
-                console.log(tasks);
-                if(tasks.length < 2){
-                    $('.deleteAll').addClass('hidden');
-                }
-            });
-            $('.check').on('click', function(){
-                $(this).toggleClass('checked');
-                $(this).parent().toggleClass('done');
-                $('.trash').css('color', 'black')
-            });
-        }   
-        
-        $('.deleteAll').click(function(){
-            $('li').remove();
-            $(this).addClass('hidden');
-        })
+        }
+
+        $('.trash').on('click', function(){
+            $(this).parent().remove();
+            tasks.pop();
+            if(tasks.length < 2){
+                $('.deleteAll').addClass('hidden');
+            }
+        });
+    
+        $('.check').click(function(){
+            if($(this).parent().hasClass('checked')){
+                $(this).parent().removeClass('checked');
+            }else $(this).parent().addClass('checked');
+        });
+    });
+
+    $('.deleteAll').click(function(){
+        $('li').remove();
+        $(this).addClass('hidden');
     });
 });
