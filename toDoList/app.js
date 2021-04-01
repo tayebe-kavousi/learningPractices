bc2cdf58801b73645e82d317af8f546d35bb056b
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-04-01 15:54:35 +0800

    fixing the buttons functionality

--- toDoList/app.js
+++ toDoList/app.js
@@ -7,7 +7,7 @@ $(function(){
     $('ul').on('click', handleCheckOrDelete);
     $('.deleteAll').on('click', handleDeleteAll);
 
-    //handlers
+    //handlers 
 
     function handelSubmitForm(event){
         event.preventDefault();
@@ -15,8 +15,8 @@ $(function(){
         if (val != ''){
             let htmlString = '<span class="toDoItem">'+ val+'</span>'
             let elem = $('<li class="toDo"></li>').html(htmlString);
-            elem.append('<i class="trash far fa-trash-alt"></i>');
-            elem.append('<i class="check fas fa-check"></i>');
+            elem.append('<i name="trash" class="trash far fa-trash-alt"></i>');
+            elem.append('<a ><i name="check" class="check fas fa-check"></i>');
             $('#myList').append(elem);
             $('#input').val('');
 
@@ -27,27 +27,31 @@ $(function(){
         }
     }
 
-    function handleCheckOrDelete(event){
-        if(event.target.hasClass('check')){
-            checkToDo(event);
+    function handleCheckOrDelete(e){
+        if($(e.target).hasClass('check')){
+            console.log("khiar")
+            checkToDo(e);
         }
-        if(event.target.hasClass('trash')){
-            deleteToDo(event);
+        if($(e.target).hasClass('trash')){
+            console.log("golabi")
+            deleteToDo(e);
         }
     }
 
-    function checkToDo(event){
-        $(this).parent().remove();
+    function deleteToDo (e){
+        let item = $(e.target).parent();
+        item.remove();
         tasks.pop();
         if(tasks.length < 2){
             $('.deleteAll').addClass('hidden');
         }
     }
 
-    function deleteToDo(event){
-        if($(this).parent().hasClass('checked')){
-            $(this).parent().removeClass('checked');
-        }else $(this).parent().addClass('checked');
+    function checkToDo(e){
+        let item = $(e.target).parent();
+        if(item.hasClass('checked')){
+            item.removeClass('checked');
+        }else item.addClass('checked');
     }
     
 
