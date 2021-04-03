3636cd96924e55bf13a429ab076d818db0439fbf
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-04-03 15:19:15 +0800

    styling + fixing buttons functionality

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,7 +1,10 @@
 
 $(function(){
 
-    let tasks;
+    let toDoTasks;
+    let doingTasks;
+    let doneTasks;
+
     //submit event on form :
     $('form').on('submit',handelSubmitForm);
     $('ol').on('click', handle_start_Delete_edit);
@@ -18,13 +21,14 @@ $(function(){
             let htmlString = '<span class="textItem">'+ val+'</span>'
             let elem = $('<li class="toDoItem"></li>').html(htmlString);
             elem.append('<i class="edit fas fa-edit"></i>');
-            elem.append('<i name="trash" class="trash far fa-trash-alt"></i>');
-            elem.append('<i name="start" class="start fas fa-check"></i>');
-            $('#myList').append(elem);
+            elem.append('<i class="trash far fa-trash-alt"></i>');
+            elem.append('<i class="start fas fa-play"></i>');
+            $('#toDoList').append(elem);
+            $('#placeHolderToDo').addClass('hidden');
             $('#input').val('');
 
-            tasks = $('li').get();
-            if(tasks.length > 1){
+            toDoTasks = $('#toDoList>li').get();
+            if(toDoTasks.length > 1){
                 $('.deleteAll').removeClass('hidden');
             }
         }
@@ -46,26 +50,40 @@ $(function(){
     function deleteToDo (e){
         let item = $(e.target).parent();
         item.remove();
-        tasks.pop();
-        if(tasks.length < 2){
+        if(item.hasClass('toDoItem')) toDoTasks.pop();
+        if(item.hasClass('doingItem')) doingTasks.pop();
+        if(item.hasClass('doneItem')) doneTasks.pop();
+        if(toDoTasks.length < 2){
             $('.deleteAll').addClass('hidden');
+        }
+        if(toDoTasks.length == 0){
+            $('#placeHolderToDo').removeClass('hidden')
+        }
+        if(doingTasks.length == 0){
+            $('#placeHolderDoing').removeClass('hidden')
+        }
+        if(doneTasks.length == 0){
+            $('#placeHolderDone').removeClass("hidden");
         }
     }
 
     function startToDo(e){
         let item = $(e.target).parent();
         item.addClass('started');
-        $('#placeHolderInProgress').remove();
-        $('.itemsInProgress').append(item);
+        $('#placeHolderDoing').addClass('hidden');
+        $('#doingList').append(item);
         item.removeClass('toDoItem');
         item.addClass('doingItem');
         item.attr('contenteditable','true');
-        $('li.started>i').remove();
-        tasks.pop();
-        console.log(tasks);
-        if(tasks.length < 2){
+        $('li.started>i.start').remove();
+        item.append('<i class="finish fas fa-check"></i>');
+        toDoTasks.pop();
+        if(toDoTasks.length == 0)$('#placeHolderToDo').removeClass('hidden');
+        if(toDoTasks.length < 2){
             $('.deleteAll').addClass('hidden');
         }
+        doingTasks = $('#doingList>li').get();
+        console.log('doing task: ' + doingTasks);
     }
      function editToDo(e){
         let item = $(e.target).parent();
@@ -75,8 +93,10 @@ $(function(){
      }
 
     function handleDeleteAll(){
-        $('#myList>li').remove();
+        $('#toDoList').remove();
         $(this).addClass('hidden');
+        $('#placeHolderDone').removeClass("hidden");
+        toDoTasks = [];
     }
     
 });
