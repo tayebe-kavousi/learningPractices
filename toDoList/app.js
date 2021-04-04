56552392647a7b31cdb1120759a7a7d280eb5e70
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-04-04 17:10:25 +0800

    add a button for clear the page and responsive design

--- toDoList/app.js
+++ toDoList/app.js
@@ -5,22 +5,24 @@ $(function(){
     let doingTasks;
     let doneTasks;
 
+
     //submit event on form :
     $('form').on('submit',handelSubmitForm);
-    $('ol').on('click', handle_start_Delete_edit);
+    $('ol').on('click', handle_start_Delete_edit_finish);
     $('.deleteAll').on('click', handleDeleteAll);
     $(document).on('dbclick', function(e){
         e.preventDefault();
     });
+    $('#clearThePage').on('click', clearThePage);
+    
     //handlers 
 
     function handelSubmitForm(event){
         event.preventDefault();
         let val = $('#input').val();
         if (val != ''){
-            let htmlString = '<span class="textItem">'+ val+'</span>'
+            let htmlString = '<span class="textItem" contenteditable="true">'+ val+'</span>'
             let elem = $('<li class="toDoItem"></li>').html(htmlString);
-            elem.append('<i class="edit fas fa-edit"></i>');
             elem.append('<i class="trash far fa-trash-alt"></i>');
             elem.append('<i class="start fas fa-play"></i>');
             $('#toDoList').append(elem);
@@ -34,7 +36,7 @@ $(function(){
         }
     }
 
-    function handle_start_Delete_edit(e){
+    function handle_start_Delete_edit_finish(e){
         let target = $(e.target);
         if(target.hasClass('start')){
             startToDo(e);
@@ -44,6 +46,9 @@ $(function(){
         }
         if(target.hasClass('edit')){
             editToDo(e);
+        }
+        if(target.hasClass('finish')){
+            finishDoing(e);
         }
     }
 
@@ -74,7 +79,6 @@ $(function(){
         $('#doingList').append(item);
         item.removeClass('toDoItem');
         item.addClass('doingItem');
-        item.attr('contenteditable','true');
         $('li.started>i.start').remove();
         item.append('<i class="finish fas fa-check"></i>');
         toDoTasks.pop();
@@ -83,13 +87,19 @@ $(function(){
             $('.deleteAll').addClass('hidden');
         }
         doingTasks = $('#doingList>li').get();
-        console.log('doing task: ' + doingTasks);
     }
-     function editToDo(e){
+
+     function finishDoing(e){
         let item = $(e.target).parent();
-        item.attr('contenteditable','true');
-        item.height('2rem');
-        item.focus();
+        item.addClass('finished');
+        $('#placeHolderDone').addClass('hidden');
+        $('#doneList').append(item);
+        item.removeClass('doingItem');
+        item.addClass('doneItem');
+        $('li.finished>i.finish').remove();
+        doingTasks.pop();
+        if(doingTasks.length == 0)$('#placeHolderDoing').removeClass('hidden');
+        doneTasks = $('#doneList>li').get();
      }
 
     function handleDeleteAll(){
@@ -97,6 +107,13 @@ $(function(){
         $(this).addClass('hidden');
         $('#placeHolderDone').removeClass("hidden");
         toDoTasks = [];
+    }
+
+    function clearThePage(e){
+        $('li').remove();
+        $('#placeHolderToDo').removeClass("hidden");
+        $('#placeHolderDoing').removeClass("hidden");
+        $('#placeHolderDone').removeClass("hidden");
     }
     
 });
