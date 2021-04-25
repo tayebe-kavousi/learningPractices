05165b43d9f6eb68d67ab836ed613e187e57a9fe
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-04-25 15:49:38 +0800

    Abort the fetch request

--- stream.js
+++ stream.js
@@ -1,5 +1,9 @@
-(async function(){
-    let response = await fetch("https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits?per_page=100");
+let controller = new AbortController();
+setTimeout(()=>controller.abort(),1000)
+try{(async function stream(){ 
+    let response = await fetch("https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits?per_page=100",
+        {'signal' : controller.signal}
+    );
     let reader = response.body.getReader();
     let recievedLength = 0;
     while(true){
@@ -9,4 +13,11 @@
     }
     let size= document.getElementById("size");
     size.innerHTML = recievedLength;
-})()
+})()}
+catch(e){
+    if(e.name == 'AbortError'){
+        alert('aborted');
+    } else{
+        throw e;
+    }
+}
